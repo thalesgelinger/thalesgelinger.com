7771ff4b5c55fc3cffc1b781fc83b4937e2cd296
Thales Gelinger <thalesgelinger@gmail.com>
2024-02-05 10:18:38 -0300

    update theme

--- src/site.config.ts
+++ src/site.config.ts
@@ -5,9 +5,9 @@ export const siteConfig: SiteConfig = {
   // Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
   author: "Thales Gelinger",
   // Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-  title: "Astro Theme Cactus",
+  title: "Thales Gelinger",
   // Meta property used as the default description meta property
-  description: "An opinionated starter theme for Astro",
+  description: "Thales's personal website and blog",
   // HTML lang property, found in src/layouts/Base.astro L:18
   lang: "en-GB",
   // Meta property, found in src/components/BaseHead.astro L:42
